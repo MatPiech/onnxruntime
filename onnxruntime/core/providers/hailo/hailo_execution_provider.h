889621f891cba2b02c448e9e023684a3c27eb076
Mateusz <piechocki.mateusz97@gmail.com>
2025-08-16 18:07:35 +0200

    Build fixes

--- onnxruntime/core/providers/hailo/hailo_execution_provider.h
+++ onnxruntime/core/providers/hailo/hailo_execution_provider.h
@@ -30,7 +30,9 @@ public:
 
     std::vector<std::unique_ptr<ComputeCapability>>
     GetCapability(const onnxruntime::GraphViewer& graph,
-                  const IKernelLookup& /*kernel_registries*/) const override;
+                  const IKernelLookup& /*kernel_lookup*/,
+                  const GraphOptimizerRegistry& /* graph_optimizer_registry */,
+                  IResourceAccountant* resource_accountant) const override;
 
     std::shared_ptr<KernelRegistry> GetKernelRegistry() const override;
 
--- onnxruntime/test/perftest/ort_test_session.cc
+++ onnxruntime/test/perftest/ort_test_session.cc
@@ -555,7 +555,7 @@ select from 'TF8', 'TF16', 'UINT8', 'FLOAT', 'ITENSOR'. \n)");
 #else
     ORT_THROW("VitisAI is not supported in this build\n");
 #endif
-  } else if (provider_name == onnxruntime::kHailoExecutionProvider) {
+  } else if (provider_name_ == onnxruntime::kHailoExecutionProvider) {
 #ifdef USE_HAILO
     Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_Hailo(session_options,
                                                                      performance_test_config.run_config.enable_cpu_mem_arena ? 1 : 0));
